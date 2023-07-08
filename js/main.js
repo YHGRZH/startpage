ee5401341e43f537b63b04108caf8ece5d9a5161
YHGRZH <107395243+YHGRZH@users.noreply.github.com>
2023-07-09 00:24:53 +0800

    main.js

--- js/main.js
+++ js/main.js
@@ -24,7 +24,7 @@ window.addEventListener('load', function () {
     setTimeout(function () {
         iziToast.show({
             title: hello,
-            message: '欢迎来到 起始页'
+            message: '欢迎来到搜索页'
         });
     }, 800);
 
